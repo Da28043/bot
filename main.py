83f90339ada123d9e097c84da0fe9fdbe220ff79
Da28043 <dak28041973@gmail.com>
2024-09-20 19:26:26 +0300

    Dockerfill

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def echo(client, message):
         await message.reply('Привет')
     elif message.text.lower() == 'пока':
         await message.reply('Пока')
-    elif message.text.lower() == 'как дела?':
+    elif message.text.lower() == 'как дела?' or message.text.lower() == 'как дела':
         await message.reply('Нормально')
     else:
         await message.reply(f'Ты написал: {message.text}')
