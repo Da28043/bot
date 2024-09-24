7e53838c2a831ad9eb79cead2243f72578b5ce8d
Da28043 <dak28041973@gmail.com>
2024-09-24 19:22:10 +0300

    Dockerfill

--- keyboards.py
+++ keyboards.py
@@ -4,6 +4,7 @@ from pyrogram import emoji
 btn_info = KeyboardButton(f'{emoji.INFORMATION} Инфо')
 btn_games = KeyboardButton(f'{emoji.VIDEO_GAME} Игры')
 btn_profile = KeyboardButton(f'{emoji.PERSON} Профиль')
+btn_time = KeyboardButton(f'{emoji.TIMER_CLOCK} Время')
 
 kb_main = ReplyKeyboardMarkup(
     keyboard=[
